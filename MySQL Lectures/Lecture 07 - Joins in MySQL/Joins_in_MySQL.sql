6da0c55f61cbc505949c34b3655ed1cb4cc3096e
Bhavesh Joshi <classwithbhavesh@gmail.com>
2026-03-20 11:50:40 +0530

    join started

---
+++ MySQL Lectures/Lecture 07 - Joins in MySQL/Joins_in_MySQL.sql
@@ -0,0 +1,55 @@
+-- Joins
+USE allinonedb;
+CREATE TABLE department(
+	id INT PRIMARY KEY,
+    dept_name VARCHAR(20),
+    dept_course VARCHAR(10)
+);
+
+CREATE TABLE teacher(
+	id INT PRIMARY KEY,
+    dept_id INT,
+    CONSTRAINT fk_dept FOREIGN KEY (dept_id) REFERENCES department(id),
+    teacher_name VARCHAR(30)
+);
+
+
+INSERT INTO department(id, dept_name, dept_course)
+VALUES 
+(121, "Science", "B.Sc."),
+(122, "Commerce", "B.Comm"),
+(144, "Literature", "BA"),
+(168, "P.E.", "P.E.");
+
+DROP TABLE teacher;
+INSERT INTO teacher(id, dept_id, teacher_name)
+VALUES 
+(12124, 168, "Sanjay Goud"),
+(12178, 144, "Preeti Verma"),
+(12196, 121, "Bhagwat Sharma"),
+(12132, 121, "Vikrant Soni"),
+(12487, 144, "Surbhi Vishwakarma"),
+(12426, 121, "Yogesh Nema");
+
+SELECT * FROM department;
+SELECT * FROM teacher;
+
+DESC department;
+DESC teacher;
+
+
+
+-- Inner Join - 
+SELECT * FROM department
+INNER JOIN teacher
+ON department.id = teacher.dept_id;
+
+SELECT * FROM department;
+SELECT * FROM teacher;
+
+
+
+-- OTUER JOIN - LEFT JOIN
+SELECT * FROM department
+LEFT JOIN teacher
+ON department.id = teacher.dept_id;
