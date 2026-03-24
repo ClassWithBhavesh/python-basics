a7b6d88e293769488c31b7cc1e9204d72037a789
Bhavesh Joshi <classwithbhavesh@gmail.com>
2026-03-24 11:21:14 +0530

    Joined covered in MySQL

--- MySQL Lectures/Lecture 07 - Joins in MySQL/Joins_in_MySQL.sql
+++ MySQL Lectures/Lecture 07 - Joins in MySQL/Joins_in_MySQL.sql
@@ -77,4 +77,94 @@ SELECT * FROM department d
 RIGHT JOIN teacher t
 ON d.id = t.dept_id;
 
--- cross join & self join
+
+
+-- self join
+CREATE TABLE des(
+	id INT PRIMARY KEY,
+    des_name VARCHAR(30),
+    dept_id INT,
+    CONSTRAINT fk_key FOREIGN KEY (dept_id) REFERENCES department(id)
+);
+
+DROP TABLE des;
+
+INSERT INTO des(id, des_name, dept_id)
+VALUES 
+(201, "Principal", 121),
+(458, "HOD1", 121),
+(423, "HOD2", 122),
+(144, "Professor", 168),
+(121, "Assistant Professor", 144),
+(122, "Associate Professor", 144);
+
+SELECT * FROM des;
+SELECT * FROM department;
+
+SELECT p.des_name AS Professor, h.des_name AS HOD
+FROM des p
+JOIN des h
+ON p.id = h.dept_id;
+
+
+CREATE TABLE emp(
+	id INT PRIMARY KEY,
+    name VARCHAR(30),
+    designation VARCHAR(30),
+    manager_id INT
+);
+SELECT * FROM emp;
+
+
+INSERT INTO emp (id, name, designation, manager_id)
+VALUE 
+(1, "Rakesh Kumar", "CEO", NULL),
+(2, "Aryan Singhniya", "CTO", 1),
+(3, "Sneha Choudhary", "HR Manager", 1),
+(4, "Rohit Pandey", "Senior Analyst", 2),
+(5, "Ranjeeta Mandal", "Junior Analyst", 4),
+(6, "Bhavya Sharma", "Junior Analyst", 4),
+(7, "Bhagyashree Iyer", "Data Analyst Intern", 3);
+
+SELECT e.name AS Employees, e.designation AS Emp_Roles, m.name AS Managers, m.designation AS Manager_Roles
+FROM emp e
+JOIN emp m
+ON e.manager_id = m.id;
+
+SELECT * FROM emp;
+
+
+
+-- Cross Join
+
+CREATE TABLE subjects (
+	id INT PRIMARY KEY,
+    subject_name VARCHAR(30)
+);
+
+CREATE TABLE exams_slot(
+	id INT PRIMARY KEY,
+    slot_time VARCHAR(30)
+);
+
+INSERT INTO subjects(id, subject_name)
+VALUES 
+(1, "English"),
+(2, "Hindi"),
+(3, "Science"),
+(4, "Maths"),
+(5, "SST");
+
+INSERT INTO exams_slot(id, slot_time)
+VALUES 
+(1, "Morning"),
+(2, "Evening"),
+(3, "Aftenoon");
+
+
+SELECT subject_name, slot_time 
+FROM subjects 
+CROSS JOIN exams_slot;
+
+
+
